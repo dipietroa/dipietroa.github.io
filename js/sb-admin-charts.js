ba08ee7b933ef43991c1a6a5957ddf80670235bc
Di Pietro Adrian <adrian.dipietro@heig-vd.ch>
2017-10-18 22:10:34 +0200

    Add the first dynamic graphic that uses the json file generated by our agent

--- js/sb-admin-charts.js
+++ js/sb-admin-charts.js
@@ -3,55 +3,92 @@
 Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#292b2c';
 // -- Area Chart Example
-var ctx = document.getElementById("myAreaChart");
-var myLineChart = new Chart(ctx, {
-  type: 'line',
-  data: {
-    labels: ["Mar 1", "Mar 2", "Mar 3", "Mar 4", "Mar 5", "Mar 6", "Mar 7", "Mar 8", "Mar 9", "Mar 10", "Mar 11", "Mar 12", "Mar 13"],
-    datasets: [{
-      label: "Sessions",
-      lineTension: 0.3,
-      backgroundColor: "rgba(2,117,216,0.2)",
-      borderColor: "rgba(2,117,216,1)",
-      pointRadius: 5,
-      pointBackgroundColor: "rgba(2,117,216,1)",
-      pointBorderColor: "rgba(255,255,255,0.8)",
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: "rgba(2,117,216,1)",
-      pointHitRadius: 20,
-      pointBorderWidth: 2,
-      data: [10000, 30162, 26263, 18394, 18287, 28682, 31274, 33259, 25849, 24159, 32651, 31984, 38451],
-    }],
-  },
-  options: {
-    scales: {
-      xAxes: [{
-        time: {
-          unit: 'date'
-        },
-        gridLines: {
-          display: false
-        },
-        ticks: {
-          maxTicksLimit: 7
-        }
-      }],
-      yAxes: [{
-        ticks: {
-          min: 0,
-          max: 40000,
-          maxTicksLimit: 5
-        },
-        gridLines: {
-          color: "rgba(0, 0, 0, .125)",
-        }
-      }],
-    },
-    legend: {
-      display: false
-    }
+var request = new XMLHttpRequest();
+request.open("GET", "generated_files/spring-projects-spring-boot.json", true);
+request.send(null);
+request.onreadystatechange = () => {
+  if ( request.readyState === 4 && request.status === 200 ) {
+    var json = JSON.parse(request.responseText);
+	var dlabels = [];
+	var dtotal = [];
+	var dmerged = [];
+	
+	Object.keys(json.datePRInfos).forEach((key, index) => {
+		dlabels.push(key);
+		dtotal.push(json.datePRInfos[key].total);
+		dmerged.push(json.datePRInfos[key].merged);
+    });
+	
+	console.log(dlabels);
+	console.log(dtotal);
+	console.log(dmerged);
+	
+	document.getElementById("areaChartDescription").innerHTML = `<b>Owner:</b> ${json.cowner} <b>Repository:</b> ${json.crepo}`;
+    var ctx = document.getElementById("myAreaChart");
+	var myLineChart = new Chart(ctx, {
+	  type: 'line',
+	  data: {
+		labels: dlabels,
+		datasets: [{
+		  label: "Total pull requests",
+		  lineTension: 0.3,
+		  backgroundColor: "rgba(2,117,216,0.2)",
+		  borderColor: "rgba(2,117,216,1)",
+		  pointRadius: 5,
+		  pointBackgroundColor: "rgba(2,117,216,1)",
+		  pointBorderColor: "rgba(255,255,255,0.8)",
+		  pointHoverRadius: 5,
+		  pointHoverBackgroundColor: "rgba(2,117,216,1)",
+		  pointHitRadius: 20,
+		  pointBorderWidth: 2,
+		  data: dtotal,
+		}, { label: "pull requests merged",
+		  lineTension: 0.3,
+		  backgroundColor: "rgba(255, 0, 0, 0.3)",
+		  borderColor: "rgba(255, 0, 0, 1)",
+		  pointRadius: 5,
+		  pointBackgroundColor: "rgba(255, 0, 0, 1)",
+		  pointBorderColor: "rgba(255,255,255,0.8)",
+		  pointHoverRadius: 5,
+		  pointHoverBackgroundColor: "rgba(255, 0, 0, 1)",
+		  pointHitRadius: 20,
+		  pointBorderWidth: 2,
+		  data: dmerged,
+		}],
+	  },
+	  options: {
+		scales: {
+		  xAxes: [{
+			time: {
+			  unit: 'date'
+			},
+			gridLines: {
+			  display: false
+			},
+			ticks: {
+			  maxTicksLimit: 7
+			}
+		  }],
+		  yAxes: [{
+			ticks: {
+			  min: 0,
+			  max: 100,
+			  maxTicksLimit: 5
+			},
+			gridLines: {
+			  color: "rgba(0, 0, 0, .125)",
+			}
+		  }],
+		},
+		legend: {
+		  display: false
+		}
+	  }
+	});
   }
-});
+}
+
+
 // -- Bar Chart Example
 var ctx = document.getElementById("myBarChart");
 var myLineChart = new Chart(ctx, {
