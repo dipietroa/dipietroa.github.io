6438df21d2dad27e4380ce93141214d2e04b2e46
Di Pietro Adrian <adrian.dipietro@heig-vd.ch>
2017-10-19 20:17:36 +0200

    add some charts to the main page

--- js/sb-admin-charts.js
+++ js/sb-admin-charts.js
@@ -3,143 +3,151 @@
 Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#292b2c';
 // -- Area Chart Example
-var request = new XMLHttpRequest();
-request.open("GET", "generated_files/spring-projects-spring-boot.json", true);
-request.send(null);
-request.onreadystatechange = () => {
-  if ( request.readyState === 4 && request.status === 200 ) {
-    var json = JSON.parse(request.responseText);
-	var dlabels = [];
-	var dtotal = [];
-	var dmerged = [];
-	
-	Object.keys(json.datePRInfos).forEach((key, index) => {
-		dlabels.push(key);
-		dtotal.push(json.datePRInfos[key].total);
-		dmerged.push(json.datePRInfos[key].merged);
-    });
-	
-	console.log(dlabels);
-	console.log(dtotal);
-	console.log(dmerged);
-	
-	document.getElementById("areaChartDescription").innerHTML = `<b>Owner:</b> ${json.cowner} <b>Repository:</b> ${json.crepo}`;
-    var ctx = document.getElementById("myAreaChart");
-	var myLineChart = new Chart(ctx, {
-	  type: 'line',
-	  data: {
-		labels: dlabels,
-		datasets: [{
-		  label: "Total pull requests",
-		  lineTension: 0.3,
-		  backgroundColor: "rgba(2,117,216,0.2)",
-		  borderColor: "rgba(2,117,216,1)",
-		  pointRadius: 5,
-		  pointBackgroundColor: "rgba(2,117,216,1)",
-		  pointBorderColor: "rgba(255,255,255,0.8)",
-		  pointHoverRadius: 5,
-		  pointHoverBackgroundColor: "rgba(2,117,216,1)",
-		  pointHitRadius: 20,
-		  pointBorderWidth: 2,
-		  data: dtotal,
-		}, { label: "pull requests merged",
-		  lineTension: 0.3,
-		  backgroundColor: "rgba(255, 0, 0, 0.3)",
-		  borderColor: "rgba(255, 0, 0, 1)",
-		  pointRadius: 5,
-		  pointBackgroundColor: "rgba(255, 0, 0, 1)",
-		  pointBorderColor: "rgba(255,255,255,0.8)",
-		  pointHoverRadius: 5,
-		  pointHoverBackgroundColor: "rgba(255, 0, 0, 1)",
-		  pointHitRadius: 20,
-		  pointBorderWidth: 2,
-		  data: dmerged,
-		}],
-	  },
-	  options: {
-		scales: {
-		  xAxes: [{
-			time: {
-			  unit: 'date'
-			},
-			gridLines: {
-			  display: false
-			},
-			ticks: {
-			  maxTicksLimit: 7
+function displayCharts() {
+	const request = new XMLHttpRequest();
+	request.open("GET", "generated_files/spring-projects-spring-boot.json", true);
+	request.send(null);
+	request.onreadystatechange = () => {
+		if ( request.readyState === 4 && request.status === 200 ) {
+			const json = JSON.parse(request.responseText);
+			const dlabels = [];
+			const dtotal = [];
+			const dmerged = [];
+			const dlu = new Date(json.lastUpdate);
+			
+			Object.keys(json.datePRInfos).forEach((key, index) => {
+				dlabels.unshift(key);
+				dtotal.unshift(json.datePRInfos[key].total);
+				dmerged.unshift(json.datePRInfos[key].merged);
+			});
+			
+			const maxArray = Math.max.apply(Math, dtotal);
+			
+			document.getElementById("areaChartDescription").innerHTML = `<b>Owner:</b> ${json.cowner} <b>Repository:</b> ${json.crepo} - PR (not merged and merged) in time`;
+			document.getElementById("areaChartLastUpdate").innerHTML = `Update: ${dlu.getDate()}/${dlu.getMonth() + 1}/${dlu.getFullYear()} at ${dlu.getHours()}:${dlu.getMinutes()}`;
+			document.getElementById("pieChartLastUpdate").innerHTML = `Update: ${dlu.getDate()}/${dlu.getMonth() + 1}/${dlu.getFullYear()} at ${dlu.getHours()}:${dlu.getMinutes()}`;
+			document.getElementById("pieChartLastUpdate2").innerHTML = `Update: ${dlu.getDate()}/${dlu.getMonth() + 1}/${dlu.getFullYear()} at ${dlu.getHours()}:${dlu.getMinutes()}`;
+			let ctx = document.getElementById("myAreaChart");
+			const myLineChart = new Chart(ctx, {
+				type: 'line',
+				data: {
+					labels: dlabels,
+					datasets: [{
+						label: "Total pull requests",
+						lineTension: 0.3,
+						backgroundColor: "rgba(2,117,216,0.2)",
+						borderColor: "rgba(2,117,216,1)",
+						pointRadius: 5,
+						pointBackgroundColor: "rgba(2,117,216,1)",
+						pointBorderColor: "rgba(255,255,255,0.8)",
+						pointHoverRadius: 5,
+						pointHoverBackgroundColor: "rgba(2,117,216,1)",
+						pointHitRadius: 20,
+						pointBorderWidth: 2,
+						data: dtotal,
+					}, { label: "pull requests merged",
+						lineTension: 0.3,
+						backgroundColor: "rgba(255, 0, 0, 0.3)",
+						borderColor: "rgba(255, 0, 0, 1)",
+						pointRadius: 5,
+						pointBackgroundColor: "rgba(255, 0, 0, 1)",
+						pointBorderColor: "rgba(255,255,255,0.8)",
+						pointHoverRadius: 5,
+						pointHoverBackgroundColor: "rgba(255, 0, 0, 1)",
+						pointHitRadius: 20,
+						pointBorderWidth: 2,
+						data: dmerged,
+					}],
+				},
+				options: {
+					scales: {
+						xAxes: [{
+						time: {
+							unit: 'date'
+						},
+						gridLines: {
+							display: false
+						},
+						ticks: {
+							maxTicksLimit: 7
+						}
+						}],
+						yAxes: [{
+						ticks: {
+							min: 0,
+							max: maxArray,
+							maxTicksLimit: 5
+						},
+						gridLines: {
+							color: "rgba(0, 0, 0, .125)",
+						}
+						}],
+					},
+					legend: {
+						display: false
+					}
+				}
+			});
+			
+			let percentMerged = dmerged.reduce((a, b) => { return a + b; }, 0);
+			percentMerged = (percentMerged / json.totalPR) * 100;
+			const percentNotMerged = 100 - percentMerged;
+			
+			ctx = document.getElementById("myPieChart");
+			const myPieChart = new Chart(ctx, {
+				type: 'pie',
+				data: {
+					labels: ['Merged PR', 'Not merged PR'],
+					datasets: [{
+						data: [percentMerged, percentNotMerged],
+						backgroundColor: ['#007bff', '#dc3545'],
+					}],
+				},
+			});
+			
+			const usernames = [];
+			const contribUsernames = [];
+			const imagePath = [];
+			const userTotal = [];
+			const userMerged = [];
+			const colors = [];
+			
+			Object.keys(json.userPRInfos).forEach((key, index) => {
+				usernames.push(key);
+				imagePath.push(json.userPRInfos[key].avatarUrl);
+				userTotal.push(json.userPRInfos[key].total);
+				if (json.userPRInfos[key].merged != 0) {
+					userMerged.push(json.userPRInfos[key].merged);
+					contribUsernames.push(key);
+				}
+			});
+			
+			for (let i = 0; i < contribUsernames.length; i++) {
+				colors.push(`#${Math.floor(Math.random()*16777215).toString(16)}`);
 			}
-		  }],
-		  yAxes: [{
-			ticks: {
-			  min: 0,
-			  max: 100,
-			  maxTicksLimit: 5
-			},
-			gridLines: {
-			  color: "rgba(0, 0, 0, .125)",
-			}
-		  }],
-		},
-		legend: {
-		  display: false
+			
+			// -- Bar Chart Example
+			ctx = document.getElementById("myBarChart");
+			var myLineChart2 = new Chart(ctx, {
+					type: 'doughnut',
+					data: {
+							labels: contribUsernames,
+							datasets: [
+									{
+											data: userMerged,
+											backgroundColor: colors
+									}
+							]
+					},
+					options: {
+							responsive: true,
+							legend: {
+								display: false
+							}
+					}    
+			});
 		}
-	  }
-	});
-  }
+	}
 }
 
-
-// -- Bar Chart Example
-var ctx = document.getElementById("myBarChart");
-var myLineChart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ["January", "February", "March", "April", "May", "June"],
-    datasets: [{
-      label: "Revenue",
-      backgroundColor: "rgba(2,117,216,1)",
-      borderColor: "rgba(2,117,216,1)",
-      data: [4215, 5312, 6251, 7841, 9821, 14984],
-    }],
-  },
-  options: {
-    scales: {
-      xAxes: [{
-        time: {
-          unit: 'month'
-        },
-        gridLines: {
-          display: false
-        },
-        ticks: {
-          maxTicksLimit: 6
-        }
-      }],
-      yAxes: [{
-        ticks: {
-          min: 0,
-          max: 15000,
-          maxTicksLimit: 5
-        },
-        gridLines: {
-          display: true
-        }
-      }],
-    },
-    legend: {
-      display: false
-    }
-  }
-});
-// -- Pie Chart Example
-var ctx = document.getElementById("myPieChart");
-var myPieChart = new Chart(ctx, {
-  type: 'pie',
-  data: {
-    labels: ["Blue", "Red", "Yellow", "Green"],
-    datasets: [{
-      data: [12.21, 15.58, 11.25, 8.32],
-      backgroundColor: ['#007bff', '#dc3545', '#ffc107', '#28a745'],
-    }],
-  },
-});
+displayCharts();
